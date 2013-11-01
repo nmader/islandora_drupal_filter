e71491f96090ca725a72053fd5cfb1c6d3006b51
Adam Vessey <adam@discoverygarden.ca>
2013-11-01 14:27:15 -0300

    Get some actual tests together!

--- src/test/java/ca/upei/roblib/fedora/servletfilter/DrupalAuthModuleTest.java
+++ src/test/java/ca/upei/roblib/fedora/servletfilter/DrupalAuthModuleTest.java
@@ -1,5 +1,8 @@
 package ca.upei.roblib.fedora.servletfilter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.TestCase;
 
 public class DrupalAuthModuleTest extends TestCase {
@@ -10,8 +13,37 @@ public class DrupalAuthModuleTest extends TestCase {
 		
 	    mockInstance = new DrupalAuthModuleMock(this.getClass().getResourceAsStream("filter-drupal.xml"));	
 	}
+	
+	public void testFindUserUserOneHasAdministratorRole() {
+		mockInstance.findUser("alpha", "first");
+		assertTrue("User \"1\" gets the \"administrator\" role", mockInstance.attributeValues.contains("administrator"));
+	}
+	
+	public void testFindUserAnonymous() {
+		mockInstance.findUser("anonymous", "anonymous");
+		assertTrue("Anonymous gets the anonymous role", mockInstance.attributeValues.contains(DrupalAuthModule.ANONYMOUSROLE));
+	}
 
-	public void testFindUser() {
-		fail("Not yet implemented");
+	public void testFindUserAuthenticatedUser() {
+		Map<String,String> users = new HashMap<String, String>();
+		users.put("alpha", "first");
+		users.put("bravo", "second");
+		users.put("charlie", "third");
+		
+		for (String key: users.keySet()) {
+			mockInstance.findUser(key, users.get(key));
+			assertTrue("Has the \"authenticated user\" role", mockInstance.attributeValues.contains("authenticated user"));
+			assertFalse("Doesn't have the \"third role\"", mockInstance.attributeValues.contains("third role"));
+		}
+	}
+	
+	public void testFindUserConfiguredRoles() {
+		mockInstance.findUser("alpha", "first");
+		assertTrue("Alpha has proper roles", (
+				mockInstance.attributeValues.contains("first role") && 
+				mockInstance.attributeValues.contains("second role")));
+		
+		mockInstance.findUser("bravo", "second role");
+		assertTrue("Bravo has proper role", mockInstance.attributeValues.contains("second role"));
 	}
 }
